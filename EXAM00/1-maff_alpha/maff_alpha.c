7b170861eaff23202f52240abb3bafb4c3e173e4
Afwz7 <ahmedelfaouz3@gmail.com>
2022-12-10 22:23:24 -0500

    exam pool

---
+++ EXAM00/1-maff_alpha/maff_alpha.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   maff_alpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-faou <ael-faou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/10 21:59:49 by ael-faou          #+#    #+#             */
+/*   Updated: 2022/12/10 22:22:20 by ael-faou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_maff_alpha(void)
+{
+	int	a;
+	int	b;
+	int	i;
+
+	a = 97;
+	b = 65;
+	i = 1;
+	while (a <= 122 && b <= 90)
+	{
+		if (i % 2 == 1)
+		{
+			ft_putchar(a);
+		}
+		else
+		{
+			ft_putchar(b);
+		}
+		a++;
+		b++;
+		i++;
+	}
+}
+
+int	main(void)
+{
+	ft_maff_alpha();
+	return (0);
+}
